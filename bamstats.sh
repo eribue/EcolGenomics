9f664fb90030c56854d617b10f9ab02184093a1c
eribue <42782508+eribue@users.noreply.github.com>
2020-02-16 20:14:42 -0500

    sunday

---
+++ bamstats.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash/
+#set repo
+myrepo="/users/e/m/embueno/EcolGenomics"
+mypop="CRA"
+output="data/project_data/RS_ExomeSeq/mapping"
+#create a header
+echo "Num.reads R1 R2 Paired MateMapped Singletons 
+MateMappedDiffChr" > ${myrepo}/myresults/${mypop}.flagstats.txt
+for file in ${output}/BWA/${mypop}*CRA.sorted.rmdup.bam
+	do 
+	f=${file/.sorted.rmdup.bam/}
+	name=`basename ${f}`
+	echo ${name} >> ${myrepo}/myresults/${mypop}.names.txt
+	samtools flagstat ${file} | awk 'NR>=6&&NR<=12' {print $1}' | column -x
+	done >> ${myrepo}/myresults/${mypop}.flagstats.txt
+
